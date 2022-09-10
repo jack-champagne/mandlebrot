83fe064cf019df527fef4944afb5396e512cd66d
Jack Champagne <jackchampagne.r@gmail.com>
2022-09-10 15:56:49 -0400

    Fix rendering algorithm
    Rendering was done poorly, defining strict transformations based upon
    pixel -> coord and coord -> pixel would be useful to have.
    
    Signed-off-by: Jack Champagne <jackchampagne.r@gmail.com>

--- mandlebrot.py
+++ mandlebrot.py
@@ -1,10 +1,15 @@
 from tkinter import LEFT, Frame, Button, Canvas, Tk
 import time
-import math
 
 def color_to_hex(r, g, b):
     return '#' + hex(r * 256 * 256 + g * 256 + b).replace('0x', '').zfill(6)
 
+def color_clip(num):
+    if num < 0: return 0
+    return num if num < 255 else 255
+
+CANVAS_HEIGHT =  1000
+CANVAS_WIDTH = int(1.5 * CANVAS_HEIGHT)
 
 class Mandlebrot:
     def __init__(self, master):
@@ -19,14 +24,14 @@ class Mandlebrot:
         )
         self.button.pack(side=LEFT)
 
-        self.graph_area = Canvas(master, width=1800, height=900)
+        self.graph_area = Canvas(master, width=CANVAS_WIDTH, height=CANVAS_HEIGHT)
         self.graph_area.pack()
 
         # Creating background
-        self.graph_area.create_rectangle(0, 0, self.graph_area.winfo_screenwidth(), self.graph_area.winfo_screenheight(), fill='#000000')
+        self.graph_area.create_rectangle(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT, fill='#000000')
 
         # Draw coords?
-
+        self.draw_coords()
 
         # Calculate
         self.calculate = Button(frame, text='Hello', command=self.render)
@@ -37,25 +42,65 @@ class Mandlebrot:
         self.iterations = 1000
         # Working vars (add z to self for future visualizations?
 
+
+    def draw_coords(self):
+        lx1, lx2 = self.coords_to_pixel(-2, 0)
+        lx3, lx4 = self.coords_to_pixel(1, 0)
+        self.graph_area.create_line(lx1, lx2, lx3, lx4,  fill="#ffffff")
+
+        ly1, ly2 = self.coords_to_pixel(0, 1)
+        ly3, ly4 = self.coords_to_pixel(0, -1)
+        self.graph_area.create_line(ly1, ly2, ly3, ly4, fill="#ffffff")
+
+    def pixel_to_coords(self, px, py):
+        ax = px / CANVAS_WIDTH
+        ay = py / CANVAS_HEIGHT
+
+        ax = ax * 3 - 2
+        ay = ay * 2 - 1
+        return (ax, ay)
+
+    def coords_to_pixel(self, cx, cy):
+        cx = (cx + 2) / 3
+        cy = (cy + 1) / 2
+
+        return (cx * CANVAS_WIDTH, cy * CANVAS_HEIGHT)
+
+
     def render(self):
         print("help!")
         start_time = time.thread_time()
-        for x in range(self.graph_area.winfo_screenwidth()):
-            for y in range(self.graph_area.winfo_screenheight()):
+        for x in range(self.graph_area.winfo_width()):
+            for y in range(self.graph_area.winfo_height()):
                 self.draw_pixel(x=x, y=y)
         end_time = time.thread_time()
         print("Done in " + str(end_time - start_time) + " secs")
+        self.draw_coords()
+
+
 
     def pixel_color(self, x, y):
-        ax = x / self.graph_area.winfo_screenwidth()
-        ay = y / self.graph_area.winfo_screenheight()
-        col = color_to_hex(math.floor((math.sin(50 * math.pi * ax) + 1) * 255/2), math.floor((math.sin(50 * math.pi * ay) + 1) * 255/2), 10)
+        ax, ay = self.pixel_to_coords(x, y)
+
+#         c = complex(x, y)
+# -        z = complex(0, 0)
+# -        i = 0
+# -        while i < self.iterations:
+# -            if (z.imag ** 2 + z.real ** 2) < (2 ** 2):
+# -                z = z ** 2 + c
+# -                i += 1
+# -            else:
+# -                return False
+# -        return True
+
+        r, g, b = (int(color_clip(50 * (ax * ax + ay * ay))), int(color_clip(50 * (ax * ax + ay * ay))), int(color_clip(50 * (ax * ax + ay * ay))))
+
+        col = color_to_hex(r, g, b)
 
         return col
 
-    def draw_pixel(self, x, y):
-            self.graph_area.create_rectangle(x, y, x + 1, y + 1, outline=self.pixel_color(x, y), fill=self.pixel_color(x, y))
-
+    def draw_pixel(self, x, y): # draws pixels in reverse order
+            self.graph_area.create_rectangle(x, self.graph_area.winfo_height() - y, x, self.graph_area.winfo_height() - y, outline=self.pixel_color(x, y), fill=self.pixel_color(x, y))
 
 root = Tk()
 app = Mandlebrot(root)
@@ -64,3 +109,4 @@ root.destroy()
 
 # Cool! I should multi-thread this! If only I could use SIMD in python (is there a way??)
 # I definitely need to do this in C++ next.
+
