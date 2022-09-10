04543408c5df0e04721028edf30ba966acd0e6f1
Jack Champagne <jackchampagne.r@gmail.com>
2022-09-10 18:43:12 -0400

    MVP for iterations coloring
    Signed-off-by: Jack Champagne <jackchampagne.r@gmail.com>

--- mandlebrot.py
+++ mandlebrot.py
@@ -1,3 +1,5 @@
+from colorsys import hsv_to_rgb
+import random
 from tkinter import LEFT, Frame, Button, Canvas, Tk
 import time
 
@@ -6,10 +8,12 @@ def color_to_hex(r, g, b):
 
 def color_clip(num):
     if num < 0: return 0
-    return num if num < 255 else 255
+    return int(num) if num < 255 else 255
 
-CANVAS_HEIGHT =  1000
+CANVAS_HEIGHT =  1750
 CANVAS_WIDTH = int(1.5 * CANVAS_HEIGHT)
+
+SCALE = 2
 
 class Mandlebrot:
     def __init__(self, master):
@@ -39,30 +43,30 @@ class Mandlebrot:
 
         # Constants
         self.x, self.y = 10, 10
-        self.iterations = 1000
+        self.iterations = 2000
         # Working vars (add z to self for future visualizations?
 
 
     def draw_coords(self):
-        lx1, lx2 = self.coords_to_pixel(-2, 0)
-        lx3, lx4 = self.coords_to_pixel(1, 0)
+        lx1, lx2 = self.coords_to_pixel(-2 * SCALE, 0)
+        lx3, lx4 = self.coords_to_pixel(1 * SCALE, 0)
         self.graph_area.create_line(lx1, lx2, lx3, lx4,  fill="#ffffff")
 
-        ly1, ly2 = self.coords_to_pixel(0, 1)
-        ly3, ly4 = self.coords_to_pixel(0, -1)
+        ly1, ly2 = self.coords_to_pixel(0, 1 * SCALE)
+        ly3, ly4 = self.coords_to_pixel(0, -1 * SCALE)
         self.graph_area.create_line(ly1, ly2, ly3, ly4, fill="#ffffff")
 
     def pixel_to_coords(self, px, py):
         ax = px / CANVAS_WIDTH
         ay = py / CANVAS_HEIGHT
 
-        ax = ax * 3 - 2
-        ay = ay * 2 - 1
+        ax = (ax * 3 - 2)*SCALE
+        ay = (ay * 2 - 1)*SCALE
         return (ax, ay)
 
     def coords_to_pixel(self, cx, cy):
-        cx = (cx + 2) / 3
-        cy = (cy + 1) / 2
+        cx = (cx/SCALE + 2) / 3
+        cy = (cy/SCALE + 1) / 2
 
         return (cx * CANVAS_WIDTH, cy * CANVAS_HEIGHT)
 
@@ -82,6 +86,19 @@ class Mandlebrot:
     def pixel_color(self, x, y):
         ax, ay = self.pixel_to_coords(x, y)
 
+        rf, gf, bf = (0, 0, 0)
+        c = complex(ax, ay)
+        z = c
+        for _i in range(self.iterations):
+            if abs(z) > 2:
+                rf, gf, bf = hsv_to_rgb((_i % 255 / 255) ** 0.5, 1.0, 1.0)
+                break
+            z = z ** 2 + c
+            
+        col = color_to_hex(color_clip(rf * 256),color_clip(gf * 256), color_clip(bf * 256))
+
+        return col
+
 #         c = complex(x, y)
 # -        z = complex(0, 0)
 # -        i = 0
@@ -92,12 +109,6 @@ class Mandlebrot:
 # -            else:
 # -                return False
 # -        return True
-
-        r, g, b = (int(color_clip(50 * (ax * ax + ay * ay))), int(color_clip(50 * (ax * ax + ay * ay))), int(color_clip(50 * (ax * ax + ay * ay))))
-
-        col = color_to_hex(r, g, b)
-
-        return col
 
     def draw_pixel(self, x, y): # draws pixels in reverse order
             self.graph_area.create_rectangle(x, self.graph_area.winfo_height() - y, x, self.graph_area.winfo_height() - y, outline=self.pixel_color(x, y), fill=self.pixel_color(x, y))
