324f434a263b9a0e133df1fd949d5c207bc5b48f
Jack Champagne <jackchampagne.r@gmail.com>
2022-09-10 13:10:51 -0400

    Inital refactoring
    Trying to get full spectrum colors on set. Will be implementing
    iterative solution correctly.
    
    Signed-off-by: Jack Champagne <jackchampagne.r@gmail.com>

--- mandlebrot.py
+++ mandlebrot.py
@@ -1,6 +1,9 @@
-from tkinter import *
+from tkinter import LEFT, Frame, Button, Canvas, Tk
 import time
 import math
+
+def color_to_hex(r, g, b):
+    return '#' + hex(r * 256 * 256 + g * 256 + b).replace('0x', '').zfill(6)
 
 
 class Mandlebrot:
@@ -16,13 +19,14 @@ class Mandlebrot:
         )
         self.button.pack(side=LEFT)
 
-        self.graph_area = Canvas(master, width=750, height=500)
+        self.graph_area = Canvas(master, width=1800, height=900)
         self.graph_area.pack()
 
         # Creating background
-        self.graph_area.create_rectangle(0, 0, 750, 500, fill='#002200')
+        self.graph_area.create_rectangle(0, 0, self.graph_area.winfo_screenwidth(), self.graph_area.winfo_screenheight(), fill='#000000')
 
         # Draw coords?
+
 
         # Calculate
         self.calculate = Button(frame, text='Hello', command=self.render)
@@ -31,41 +35,26 @@ class Mandlebrot:
         # Constants
         self.x, self.y = 10, 10
         self.iterations = 1000
-        # Working vars (add z to self for future visualizations?)
-
-        # Test
-        self.draw_pixel(x=100, y=100, color=True)
-        self.draw_pixel(x=102, y=102, color=False)
-        print(self.pixel_color(x=2, y=-1))
+        # Working vars (add z to self for future visualizations?
 
     def render(self):
+        print("help!")
         start_time = time.thread_time()
-        for x in range(750):
-            for y in range(500):
-                # Do world space and pixel space conversion
-                f_x = x / 200 - 2.25
-                f_y = y / 200 - 1.25
-                self.draw_pixel(x=x, y=y, color=self.pixel_color(f_x, f_y))
+        for x in range(self.graph_area.winfo_screenwidth()):
+            for y in range(self.graph_area.winfo_screenheight()):
+                self.draw_pixel(x=x, y=y)
         end_time = time.thread_time()
         print("Done in " + str(end_time - start_time) + " secs")
 
     def pixel_color(self, x, y):
-        c = complex(x, y)
-        z = complex(0, 0)
-        i = 0
-        while i < self.iterations:
-            if (z.imag ** 2 + z.real ** 2) < (2 ** 2):
-                z = z ** 2 + c
-                i += 1
-            else:
-                return False
-        return True
+        ax = x / self.graph_area.winfo_screenwidth()
+        ay = y / self.graph_area.winfo_screenheight()
+        col = color_to_hex(math.floor((math.sin(50 * math.pi * ax) + 1) * 255/2), math.floor((math.sin(50 * math.pi * ay) + 1) * 255/2), 10)
 
-    def draw_pixel(self, x, y, color):
-        if color:
-            self.graph_area.create_rectangle(x, y, x + 1, y + 1, outline='white')
-        else:
-            self.graph_area.create_rectangle(x, y, x + 1, y + 1, outline='black')
+        return col
+
+    def draw_pixel(self, x, y):
+            self.graph_area.create_rectangle(x, y, x + 1, y + 1, outline=self.pixel_color(x, y), fill=self.pixel_color(x, y))
 
 
 root = Tk()
